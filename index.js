48b163e5b03d7dae2d69555d78dc18ce512731e3
wsavd <tuve@rootfest.net>
2017-06-04 13:54:27 +0300

    a

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var db = 'mongodb://admin:admin@ds062339.mlab.com:62339/piperdb'
-mongoose.connect(db);
+app.set('database', (process.env.MONGODB_URI || 'mongodb://admin:admin@ds062339.mlab.com:62339/piperdb'));
+//const uri = 'localhost/books';
+mongoose.connect(app.get('database'))
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
 
 const morgan = require('morgan');
 app.use(morgan('dev'));
